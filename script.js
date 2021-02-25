57d8475579604c5017a9a6e321820585c024d0b8
Владимирова Екатерина <echapyrkina@mail.ru>
2021-02-25 21:15:18 +0400

    lesson 7

--- script.js
+++ script.js
@@ -1,22 +1,55 @@
-let num;
-let score;
-let questions = ['Земля круглая или плоская?', 'Сколько месяцев в году?', '2 + 2 =?', 'Как зовут внучку Деда Мороза?'];
-let answers = ['круглая', '12', '4', 'снегурочка'];
-let currentResult = 0;
-let answer;
- function correctAnswer() {
-   questions.forEach((item) => { 
- answer = prompt(item);
-answers.forEach((item) => {
-   	if (answer == item) {
-   		currentResult++;
-   	}
-   })
-})
+let num = 0;
+let score = 0;
+let question1 = { question: 'Планета Земля круглая?', correctAnswer: 'да' };
+let question2 = { question: 'В году 12 месяцев?', correctAnswer: 'да' };
+let question3 = { question: '2 + 2 = 22?', correctAnswer: 'нет' };
+let question4 = { question: 'Помидор это фрукт?', correctAnswer: 'нет' };
+let questions = [question1, question2, question3, question4];
 
-  	}
+buildQuiz = (questions) => {
+	questions.forEach((item) => {
+		let question = document.getElementById('question');
+		question.innerHTML += ' ' + item.question
+		question.style.color = 'blue';
+	})
+}
+
+buildQuiz(questions);
+
+function showResults() {
+	questions.forEach((item) => {
+		let i = prompt(item.questions);
+		if (item.correctAnswer.toLowerCase() === i.toLowerCase()) {
+			score++;
+		}
+	})
+	let answer = document.querySelector('.correctAnswer');
+	answer.innerHTML = 'Правильные ответы - : ' + score;
+}
 
 
-correctAnswer(questions, answers);
-let text = document.getElementById('text');
-text.innerHTML = ('Количество верных ответов - ' + currentResult);
+const nextButton = document.querySelector('.nextbutton');
+nextButton.addEventListener('click', (event) => {
+	console.log('button');
+});
+
+const previousButton = document.querySelector('.previousbutton');
+nextButton.addEventListener('click', (event) => {
+	console.log('button');
+});
+
+const submitButton = document.getElementById('text');
+submitButton.addEventListener('click', () => {
+	showResults();
+});
+
+let slides = document.querySelectorAll('.slider');
+let currentSlide = 0;
+function nextSlide() {
+	for (let i = 0; i < slides.length; i++) {
+		currentSlide++;
+		console.log('номер слайда - ' + currentSlide);
+	}
+
+}
+nextSlide();
