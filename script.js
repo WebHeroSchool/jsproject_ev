1510e8ac70dfe1609bcbead92056ada60c9f5eda
Владимирова Екатерина <echapyrkina@mail.ru>
2021-03-17 17:21:18 +0400

    settimeout

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 let form = document.querySelector('.form');
 let name = form.querySelector('.form-name');
-form.addEventListener('submit', function () {
+let timer;
+const submitButton = document.getElementById('submit');
+const previousButton = document.getElementById('previous');
+const nextButton = document.getElementById('next');
+let restartButton = document.getElementById('btn');
+
+form.addEventListener('submit', function (event) {
 	event.preventDefault();
-	let regex = /^[А-ЯЁ]{1}[а-яё]+$/;
+	let regex = /^[A-ZА-Я]+[a-zа-я]{2,10}$/;
 	name.classList.remove('error');
-	start();
-	form.className = 'start';
+
+
 	if (!regex.test(name.value)) {
 		event.preventDefault();
 		name.classList.add('error');
@@ -16,70 +22,15 @@ form.addEventListener('submit', function () {
 		name.parentElement.insertBefore(error, name);
 	}
 	else {
-		name.classList.remove('error');
-		let elem = document.getElementsByClassName('error-block');
-		elem[0].remove();
+		//name.classList.remove('error');
+		//let elem = document.getElementsByClassName('error-block');
+		//elem[0].remove();
+		form.style.display = "none";
+		start();
 	}
 })
 
 function start() {
-	const buildQuiz = () => {
-		const output = [];
-		questions.forEach((currentQuestion, questionNumber) => {
-			const answers = [];
-			for (let letter in currentQuestion.answers) {
-				answers.push(
-					`<label class="answer">
-					<input type="radio" class="input" name="question${questionNumber}" value="${letter}" >
-						${letter}:
-						${currentQuestion.answers[letter]}
-				</label>`
-				);
-			}
-			output.push(
-				`<div class="slide">
-			<div class="question"> ${currentQuestion.question} </div>
-			<div class="answers"> ${answers.join('')} </div>
-			</div>`
-			);
-		});
-		quizContainer.innerHTML = output.join('');
-
-	}
-
-	function func() {
-		let inputs = document.querySelectorAll(".input");
-		for (let i = 0; i < inputs.length; i++) {
-			inputs[i].disabled = true;
-		}
-	};
-	let elem = document.querySelectorAll('.input');
-
-	let numCorrect = 0;
-	function checkResult() {
-		const answerContainers = quizContainer.querySelectorAll('.answers');
-		questions.forEach((currentQuestion, questionNumber) => {
-			const answerContainer = answerContainers[questionNumber];
-			const selector = `input[name=question${questionNumber}]:checked`;
-			const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-			if (userAnswer === currentQuestion.correctAnswer) {
-				numCorrect++;
-				answerContainers[questionNumber].style.color = 'lightgreen';
-			}
-			else {
-				answerContainers[questionNumber].style.color = 'red';
-			}
-		});
-	}
-
-	function showResults() {
-		resultsContainer.innerHTML = `Правильных ответов ${numCorrect} из ${questions.length}`;
-
-	}
-
-	const quizContainer = document.querySelector('.quiz-container');
-	const resultsContainer = document.getElementById('results');
-	const submitButton = document.getElementById('submit');
 
 	const questions = [
 		{
@@ -115,11 +66,65 @@ function start() {
 			correctAnswer: "b"
 		},
 	];
+	const buildQuiz = () => {
+		const output = [];
+		questions.forEach((currentQuestion, questionNumber) => {
+			const answers = [];
+			for (let letter in currentQuestion.answers) {
+				answers.push(
+					`<label class="answer">
+					<input type="radio" class="input" name="question${questionNumber}" value="${letter}" >
+						${letter}:
+						${currentQuestion.answers[letter]}
+				</label>`
+				);
+			}
+			output.push(
+				`<div class="slide">
+			<div class="question"> ${currentQuestion.question} </div>
+			<div class="answers"> ${answers.join('')} </div>
+			</div>`
+			);
+		});
+		quizContainer.innerHTML = output.join('');
+
+	}
+	const quizContainer = document.querySelector('.quiz-container');
+	const resultsContainer = document.getElementById('results');
+
+	function func() {
+		let inputs = document.querySelectorAll(".input");
+		for (let i = 0; i < inputs.length; i++) {
+			inputs[i].disabled = true;
+		}
+	};
+	let elem = document.querySelectorAll('.input');
+
+	let numCorrect = 0;
+	function checkResult() {
+		const answerContainers = quizContainer.querySelectorAll('.answers');
+		questions.forEach((currentQuestion, questionNumber) => {
+			const answerContainer = answerContainers[questionNumber];
+			const selector = `input[name=question${questionNumber}]:checked`;
+			const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+			if (userAnswer === currentQuestion.correctAnswer) {
+				numCorrect++;
+				answerContainers[questionNumber].style.color = 'lightgreen';
+			}
+			else {
+				answerContainers[questionNumber].style.color = 'red';
+			}
+		});
+	}
+
+	function showResults() {
+		resultsContainer.innerHTML = `Правильных ответов ${numCorrect} из ${questions.length}`;
+
+	}
 
 	buildQuiz(questions);
 
-	const previousButton = document.getElementById('previous');
-	const nextButton = document.getElementById('next');
+
 	const slides = document.querySelectorAll('.slide');
 	let currentSlide = 0;
 	let isCompleted = slides.length;
@@ -128,10 +133,16 @@ function start() {
 		slides[currentSlide].classList.remove('active-slide');
 		slides[n].classList.add('active-slide');
 		currentSlide = n;
-		let timer = 10000;
-		if (timer !== null) {
-			clearTimeout(timer);
+
+		const radiobuttons = document.querySelectorAll(".answers input");
+		radiobuttons.forEach(button => button.removeAttribute("disabled"));
+
+		function blockQuizOnTimeout() {
+			radiobuttons.forEach(button => button.setAttribute("disabled", true));
+			alert("Время на ответ истекло");
 		}
+
+		if (timer !== null) clearTimeout(timer);
 		if (currentSlide < slides.length - 1 && isCompleted === null) {
 			timer = setTimeout(() => {
 				showNextSlide();
@@ -142,6 +153,7 @@ function start() {
 				blockQuizOnTimeout(currentSlide);
 			}, 10000);
 		}
+
 		if (currentSlide === 0) {
 			previousButton.style.display = 'none';
 		} else {
@@ -155,10 +167,8 @@ function start() {
 			submitButton.style = 'none';
 		}
 	}
-	function blockQuizOnTimeout() {
-		nextButton.disabled = true;
-		previousButton.disabled = true;
-	}
+
+
 	showSlide(currentSlide);
 
 	function showNextSlide() {
@@ -177,15 +187,15 @@ function start() {
 		btn.classList.remove('btn_block');
 	})
 
-	let btn = document.getElementById('btn');
+
 	function restart() {
 		resultsContainer.classList.add('result');
 		start();
 		nextButton.disabled = false;
 		previousButton.disabled = false;
-		btn.classList.add('btn_block');
+		restartButton.classList.add('btn_block');
 		showSlide(currentSlide);
 	}
-	btn.addEventListener('click', restart);
+	restartButton.addEventListener('click', restart);
 }
 
